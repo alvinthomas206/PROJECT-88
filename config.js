42b91bfb1ceb36e2657152497fe068541b760147
ALVIN THOMAS <alvinthomas206@gmail.com>
2021-07-08 22:07:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAbC2uPcUhxOSrCYnSL3kf0bdAJrXsyig4",
+    authDomain: "barter-2c505.firebaseapp.com",
+    projectId: "barter-2c505",
+    storageBucket: "barter-2c505.appspot.com",
+    messagingSenderId: "447936024092",
+    appId: "1:447936024092:web:23123b1425a669ec7ad9d9"
+
+  };
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore();
